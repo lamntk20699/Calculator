00196de153239ffb5f469a80e6520da71f85fef1
ntlam2061999 <nguyentunglam2061999@gmail.com>
2021-11-07 21:03:21 +0700

    Calculator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,254 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    Button btnAdd, btnSub, btnMulti, btnDivide, btnResult, btnMinus, btnCE, btnC;
+    ImageButton btnBack;
+
+    TextView mathDisplay;
+    TextView resultDisplay;
+
+    String mathBuffer;
+    String resultBuffer;
+    String checkMath = "";
+
+    Double buff1, buff2,result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTextView();
+        /*btn0.setOnClickListener();*/
+    }
+
+    private void initTextView() {
+        btn0 = findViewById(R.id.button_0);
+        btn1 = findViewById(R.id.button_1);
+        btn2 = findViewById(R.id.button_2);
+        btn3 = findViewById(R.id.button_3);
+        btn4 = findViewById(R.id.button_4);
+        btn5 = findViewById(R.id.button_5);
+        btn6 = findViewById(R.id.button_6);
+        btn7 = findViewById(R.id.button_7);
+        btn8 = findViewById(R.id.button_8);
+        btn9 = findViewById(R.id.button_9);
+        btnAdd = findViewById(R.id.button_add);
+        btnSub = findViewById(R.id.button_sub);
+        btnMulti = findViewById(R.id.button_multi);
+        btnDivide = findViewById(R.id.button_divide);
+        btnCE = findViewById(R.id.button_ce);
+        btnC = findViewById(R.id.button_c);
+        btnBack = findViewById(R.id.button_back);
+        btnMinus = findViewById(R.id.button_minus);
+        btnResult = findViewById(R.id.button_result);
+        mathDisplay = findViewById(R.id.math);
+        resultDisplay = findViewById(R.id.result);
+        mathDisplay.setText("");
+        resultDisplay.setText("0");
+        mathBuffer = "";
+        resultBuffer = "0";
+    }
+
+    private void setMathSpace(String givenValue) {
+        mathBuffer = mathDisplay.getText().toString();
+        mathBuffer = mathBuffer + givenValue;
+        mathDisplay.setText(mathBuffer);
+    }
+
+    private void  setResultSpace(String givenValue) {
+        /*Enable Button*/
+        if (checkMath.equals("=")) {
+            btnAdd.setEnabled(true);
+            btnSub.setEnabled(true);
+            btnMulti.setEnabled(true);
+            btnMinus.setEnabled(true);
+            btnDivide.setEnabled(true);
+            btnResult.setEnabled(true);
+            mathDisplay.setText("");
+            resultDisplay.setText("0");
+            resultDisplay.setTextSize(60);
+            checkMath = "";
+        }
+
+        resultBuffer = resultDisplay.getText().toString();
+        if (!resultBuffer.equals("0")) {
+            resultBuffer = resultBuffer + givenValue;
+        } else {
+            resultBuffer = givenValue;
+        }
+        resultDisplay.setText(resultBuffer);
+    }
+
+    public void minusOnClick(View view) {
+        resultBuffer = resultDisplay.getText().toString();
+        resultDisplay.setText("-" + resultBuffer);
+    }
+
+    public void resultOnClick(View view) {
+        resultBuffer = resultDisplay.getText().toString();
+        setMathSpace(resultBuffer);
+        setMathSpace(" =");
+        if (resultBuffer.equals("0")) {
+            if (checkMath.equals("/")) {
+                resultDisplay.setText(R.string.divide_by_zero);
+                resultDisplay.setTextSize(30);
+                /*Enable button*/
+                btnAdd.setEnabled(false);
+                btnSub.setEnabled(false);
+                btnMulti.setEnabled(false);
+                btnMinus.setEnabled(false);
+                btnDivide.setEnabled(false);
+            }
+            else {
+                switch (checkMath) {
+                    case "+": case "-":
+                        result = buff1;
+                        break;
+                    case "*":
+                        result = 0.0;
+                        break;
+                    default:
+                        result = buff1;
+                        break;
+                }
+                resultDisplay.setText(result+"");
+            }
+        }
+        else {
+            buff2 = Double.parseDouble(resultBuffer+"");
+            switch (checkMath) {
+                case "+" :
+                    result = buff1 + buff2;
+                    break;
+                case "-" :
+                    result = buff1 - buff2;
+                    break;
+                case "*" :
+                    result = buff1 * buff2;
+                    break;
+                case "/" :
+                    result = buff1 / buff2;
+                    break;
+                default:
+                    result = buff1;
+                    break;
+            }
+            resultDisplay.setText(result+"");
+        }
+        checkMath = "=";
+        btnResult.setEnabled(false);
+    }
+
+    public void clearOnClick(View view) {
+        mathDisplay.setText("");
+        resultDisplay.setText("0");
+    }
+
+    public void backOnClick(View view) {
+        resultBuffer = resultDisplay.getText().toString();
+        Integer temp = resultBuffer.length() - 1;
+        resultBuffer = resultBuffer.substring(0, temp);
+        if ( !resultBuffer.isEmpty() ) {
+            resultDisplay.setText(resultBuffer);
+        }
+        else {
+            resultDisplay.setText("0");
+        }
+    }
+
+    public void resetOnClick(View view) {
+        resultDisplay.setText("0");
+    }
+
+    public void addOnClick(View view) {
+        resultBuffer = resultDisplay.getText().toString();
+        setMathSpace(resultBuffer);
+        setMathSpace(" + ");
+        buff1 = Double.parseDouble(resultBuffer+"");
+        resultDisplay.setText("0");
+        checkMath = "+";
+        mathBuffer = "";
+        resultBuffer = "0";
+    }
+
+    public void subOnClick(View view) {
+        resultBuffer = resultDisplay.getText().toString();
+        setMathSpace(resultBuffer);
+        setMathSpace(" - ");
+        buff1 = Double.parseDouble(resultBuffer+"");
+        resultDisplay.setText("0");
+        checkMath = "-";
+        mathBuffer = "";
+        resultBuffer = "0";
+    }
+
+    public void multiOnClick(View view) {
+        resultBuffer = resultDisplay.getText().toString();
+        setMathSpace(resultBuffer);
+        setMathSpace(" * ");
+        buff1 = Double.parseDouble(resultBuffer+"");
+        resultDisplay.setText("0");
+        checkMath = "*";
+        mathBuffer = "";
+        resultBuffer = "0";
+    }
+
+    public void divideOnClick(View view) {
+        resultBuffer = resultDisplay.getText().toString();
+        setMathSpace(resultBuffer);
+        setMathSpace(" / ");
+        buff1 = Double.parseDouble(resultBuffer+"");
+        resultDisplay.setText("0");
+        checkMath = "/";
+        mathBuffer = "";
+        resultBuffer = "0";
+    }
+
+    public void zeroOnClick(View view) {
+        setResultSpace("0");
+    }
+
+    public void oneOnClick(View view) {
+        setResultSpace("1");
+    }
+
+    public void twoOnClick(View view) {
+        setResultSpace("2");
+    }
+
+    public void threeOnClick(View view) {
+        setResultSpace("3");
+    }
+
+    public void fourOnClick(View view) {
+        setResultSpace("4");
+    }
+
+    public void fiveOnClick(View view) {
+        setResultSpace("5");
+    }
+
+    public void sixOnClick(View view) {
+        setResultSpace("6");
+    }
+
+    public void sevenOnClick(View view) {
+        setResultSpace("7");
+    }
+
+    public void eightOnClick(View view) {
+        setResultSpace("8");
+    }
+
+    public void nineOnClick(View view) {
+        setResultSpace("9");
     }
 }
